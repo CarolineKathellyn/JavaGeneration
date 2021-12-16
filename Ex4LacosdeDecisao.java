8e380460ddd2977d89df81427c198cc042d88570
kathellyn.carolineas@gmail.com <95691713+CarolineKathellyn@users.noreply.github.com>
2021-12-15 22:29:09 -0300

    Desenvolvimento em Java
    Exercícios e Desafios na Linguagem Java.

---
+++ Ex4LacosdeDecisao.java
@@ -0,0 +1,24 @@
+public class Ex4LacosdeDecisao {
+    public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		double numero, calculo;
+		String texto;
+		
+		System.out.println("Digite um numero: ");
+		numero = in.nextDouble();
+		
+		if (numero % 2 == 0) {
+			calculo = Math.sqrt(numero);
+			texto = "O número " + numero + " é par e sua raiz quadrada é " + calculo;
+			
+		} else {
+			calculo = Math.pow(numero, 2);
+			texto = "O número " + numero + " é ímpar e seu quadrado é " + calculo;
+		}
+		System.out.println(texto);
+		in.close();
+	}	
+}
+    
+}
